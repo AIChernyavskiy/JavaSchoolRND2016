d9a278df44a768002ba7eea43b9a3411fb6a2316
AIChernyavskiy <aichernyavskiy@mail.ru>
2016-07-18 19:23:31 +0300

    2

--- ru/sbertech/test/lesson3/BeforeGenerics/PersonFirstLoad.java
+++ ru/sbertech/test/lesson3/BeforeGenerics/PersonFirstLoad.java
@@ -1,5 +1,13 @@
 package JavaSchoolRND2016.ru.sbertech.test.lesson3.BeforeGenerics;
 
+import JavaSchoolRND2016.ru.sbertech.test.lesson3.Person;
+
+import java.util.Collection;
+import java.util.List;
+
 public class PersonFirstLoad {
-    public static
+    public static Collection<Person> init(Collection<Person> person){
+        person.add(new Person(1l,"Байда ИльяВдадимирович","988827277"));
+
+    }
 }
